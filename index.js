6c0c31e5797becdfa9fa40f89eb3a8beeb5a1807
HeyEyh <virinci123@gmail.com>
2021-07-21 17:59:57 +0530

    The actualy code(only testing still)
    still in developement

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const Discord = require('discord.js')
+const client = new Discord.Client();
+const { prefix } = require('./config.json')
+
+
+client.on("ready", () => {
+console.log('Initialization Complete!')
+})
+
+client.on("message", msg => {
+if (msg.content === '${prefix}noob') {
+    const helpMenuEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Help Menu')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Need Help?\nWe all need help some time or the other\nDont worry, you are not alone')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+msg.channel.send(helpMenuEmbed)
+}
+else if (msg.content === 'hi'){
+   msg.channel.send('hello');
+}
+})
+
+
+client.login(process.env.BOT_TOKEN)
+
+
