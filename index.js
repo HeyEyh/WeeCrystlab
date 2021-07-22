996cd6a5a38d5659b01acb32df6b85dd7478510d
HeyEyh <virinci123@gmail.com>
2021-07-22 22:53:46 +0530

    complete restructure

--- index.js
+++ index.js
@@ -1,71 +1,35 @@
-require('dotenv').config()
-
 const fs = require('fs');
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
-const { prefix } = require('./config.json')
+client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
 	client.commands.set(command.name, command);
 }
 
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
-//log display options
-client.on("ready", () => {
-console.log('Initialization Complete!')
-console.log(`prefix set to: ${prefix}`)
-})
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
 
-client.commands = new Discord.Collection();
+	if (!client.commands.has(command)) return;
 
-client.on("message", msg => {
-if (msg.content === `${prefix}noob`) {
-    const helpMenuEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Help Menu')
-	.setDescription('List of Commands')
-	.setURL('https://github.com/HeyEyh/WeeCrystlab')
-	.setThumbnail('https://imgur.com/1d9hSsf.png')
-	.addField('wee', '```diff\n- the default prefix\n```')
-	.addField('noob', '```diff\n- displays this help menu\n```')
-	.addField('server details', '```diff\n- returns all available server details like: name, total members```')
-	.addField('server basic', '```diff\n- returns basic server name and total members```')
-	.addField('hi','```diff\n- returns "hello"\n```')
-	.setTimestamp()
-	.setFooter('From the Scientists at WeeCrystlabs', 'https://i.imgur.com/1d9hSsf.png');
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
 
-msg.channel.send(helpMenuEmbed)
-}
-else if (msg.content === `${prefix}demand rating`){
-   msg.channel.send('-profile');
-}
-else if (msg.content === `${prefix}server details`) {
-	const serverDetailsNotbasic = new Discord.MessageEmbed()
-	.addField('Server Name',`${msg.guild.name}`)
-	.addField('Total Members', `${msg.guild.memberCount}`)
-	.addField('Created At', `${msg.guild.createdAt}`)
-	.addField('Region', `${msg.guild.region}`)
-	.setFooter(`Owner of this guild: ${msg.guild.owner}`)
-
-	msg.channel.send(serverDetailsNotbasic);
-}
-
-else if (msg.content === `${prefix}server`) {
-
-	const serverDetailsbasic = new Discord.MessageEmbed()
-	.addField(`Server Name`,`${msg.guild.name}`)
-	.addField('Total Members', `${msg.guild.memberCount}`)
-	
-	msg.channel.send(serverDetailsbasic);
-}
-})
-
-client.login(process.env.BOT_TOKEN)
-
-
+client.login(token);
