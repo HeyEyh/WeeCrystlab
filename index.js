1a10c41d08c77dde3e5e046acd24ce4101d5b8aa
HeyEyh <virinci123@gmail.com>
2021-07-22 10:08:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ client.on("ready", () => {
 console.log('Initialization Complete!')
 })
 
+//help
+
 client.on("message", msg => {
 if (msg.content === `${prefix}noob`) {
     const helpMenuEmbed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle('Help Menu')
 	.setURL('https://github.com/HeyEyh/WeeCrystlab')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
 	.setDescription('Need Help?\nWe all need help some time or the other\nDont worry, you are not alone')
 	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.addField('hi', 'returna', true)
 	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+	.setFooter('From the scientists at WeeCrystlabs', 'https://i.imgur.com/wSTFkRM.png');
 
 msg.channel.send(helpMenuEmbed)
 }
-else if (msg.content === 'hi homie'){
+else if (msg.content === '`${prefix}hi`'){
    msg.channel.send('hello bruv');
 }
 })
